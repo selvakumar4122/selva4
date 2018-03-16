84fcdd3127543f0d5ac401bad10e32562f0a0aca
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-03-16 16:56:53 +0530

    Create qn56.c

---
+++ qn56.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[100];
+    int i,n=0,count=0,t=0,count1=0;
+    printf("Enter the string :");
+    scanf("%s",a);
+    n=strlen(a);
+    for(i=0;i<n;i++)
+    if((a[i]>='A'&&a[i]<='z'))
+    count=1;
+    else if(a[i]>=1&&a[i]<=9)
+    count1=1;
+    else
+    t=1;
+    if(count==1&&count1==1&&t==1)
+    printf("yes");
+    else
+    printf("No");
+}
