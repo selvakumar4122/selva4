f182bc5b4dd66027e79595735e4c6be7f7af4af0
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-03-20 16:08:28 +0530

    Update qn56.c

--- qn56.c
+++ qn56.c
@@ -8,13 +8,15 @@ void main()
     scanf("%s",a);
     n=strlen(a);
     for(i=0;i<n;i++)
+    {
     if((a[i]>='A'&&a[i]<='z'))
     count=1;
-    else if(a[i]>=1&&a[i]<=9)
+    else if(a[i]>=1||a[i]<=9)
     count1=1;
     else
     t=1;
-    if(count==1&&count1==1&&t==1)
+    }
+    if(count==1&&t==0&&count1==1)
     printf("yes");
     else
     printf("No");
